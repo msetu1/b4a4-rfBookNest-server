a8cbb62ac86a5339323868c5954aa386f1cfcdf3
msetu1 <msetu5763@gmail.com>
2025-02-19 15:45:03 +0600

    vercel json file add

--- src/app/modules/auth/auth.utils.ts
+++ src/app/modules/auth/auth.utils.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 export const createToken = (
   jwtPayload: { email: string; role: string },
   secret: string,
-  expiresIn: string | number // Accept both string (e.g., '1h') or number (in seconds)
+  expiresIn: string | number, // Accept both string (e.g., '1h') or number (in seconds)
 ) => {
   return jwt.sign(jwtPayload, secret, {
     expiresIn: expiresIn as jwt.SignOptions['expiresIn'], // Cast to ensure compatibility
